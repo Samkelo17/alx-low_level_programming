eff3f3f3bd6de7923a0b1c55888b0c547f0846c3
Charles <sammthombeni6@gmail.com>
2022-09-11 00:50:22 -0700

    push

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,12 +4,12 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
- * Return: 0 
+ *
+ * Return: Always 0 (Success) 
  */
+
 int main(0);
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-^C

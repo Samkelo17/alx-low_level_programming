c733f1a970d0441925706dd043fe729c666730c3
Charles <sammthombeni6@gmail.com>
2022-09-17 12:25:49 -0700

    print

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,31 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+/* more headers here */
+
+/* betty style doc for function here */
 int main(void)
 {
-	int n, num;
+	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/*your code goes there */
-	num = n % 10;
-	if (num > 5)
-	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, num);
-	}
-	else if (num == 0)
-	{
-	printf("Last digit of %d is %d and is 0\n", n, num);
-	}
-	else if (num < 6)
-	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, num);
-	}
+	/* your code here */
 	return (0);
 }
-
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ * 
+ * return always 0 (success
+ *
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - entry point
+* 
+* return : 0 succcess
+*/
+int main(void)
+{
+       char low;
+
+       for (low = 'a'; low<= 'z'; low++)
+       putchar(low);
+       printf("\n");
+
+       return (0);
+}

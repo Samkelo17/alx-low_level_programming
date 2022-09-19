9d4993fc16010a4d5fac7a6d615ff448957f26dd
Charles <sammthombeni6@gmail.com>
2022-09-19 03:15:12 -0700

    print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num =0; num < 10; num++)
+		printf("%d" , num);
+	putcha('\n');
+	return (0);
+}
